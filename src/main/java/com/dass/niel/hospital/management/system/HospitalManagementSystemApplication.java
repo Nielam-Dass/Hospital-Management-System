0732dacc9b5a582fbe3d7ae84a8d5d193cb0d774
Nielam Dass <nielam.dass@gmail.com>
2023-08-15 18:59:14 -0400

    Added Patient entity and repository

--- src/main/java/com/dass/niel/hospital/management/system/HospitalManagementSystemApplication.java
+++ src/main/java/com/dass/niel/hospital/management/system/HospitalManagementSystemApplication.java
@@ -1,13 +1,44 @@
 package com.dass.niel.hospital.management.system;
 
+import com.dass.niel.hospital.management.system.patient.Patient;
+import com.dass.niel.hospital.management.system.patient.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	PatientRepository patientRepository;
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args -> {
+			Patient patient = Patient.builder()
+					.firstName("Adam")
+					.lastName("Smith")
+					.dob(LocalDate.of(2000, 1, 1))
+					.sex("Male")
+					.ssn(111223333)
+					.phoneNumber("555-555-5555")
+					.insurance("XHealth")
+					.build();
+
+			patientRepository.save(patient);
+			List<Patient> patientList = patientRepository.findAll();
+			System.out.println(patientList);
+		};
+
 	}
 
 }
---
+++ src/main/java/com/dass/niel/hospital/management/system/patient/Patient.java
@@ -0,0 +1,46 @@
+package com.dass.niel.hospital.management.system.patient;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "patient",
+        uniqueConstraints = @UniqueConstraint(name = "ssn_unique", columnNames = "ssn")
+)
+public class Patient {
+    @Id
+    @SequenceGenerator(name = "patient_sequence", sequenceName = "patient_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "patient_sequence")
+    private Long patientId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "dob", nullable = false)
+    private LocalDate dob;
+
+    @Column(name = "sex", nullable = false, columnDefinition = "varchar(10)")
+    private String sex;
+
+    @Column(name = "ssn", nullable = false)
+    private Integer ssn;
+
+    @Column(name = "phone_number", nullable = false, columnDefinition = "varchar(20)")
+    private String phoneNumber;
+
+    @Column(name = "insurance")
+    private String insurance;
+}
