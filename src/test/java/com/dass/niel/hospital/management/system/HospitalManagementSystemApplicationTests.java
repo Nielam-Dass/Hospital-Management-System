ac711f3a0a371eb6fe20df4e5a1f3bf1af0535e3
Nielam Dass <nielam.dass@gmail.com>
2024-05-11 23:20:11 -0400

    Setting up spring boot testing

--- src/test/java/com/dass/niel/hospital/management/system/HospitalManagementSystemApplicationTests.java
+++ src/test/java/com/dass/niel/hospital/management/system/HospitalManagementSystemApplicationTests.java
@@ -1,13 +1,25 @@
 package com.dass.niel.hospital.management.system;
 
+import com.dass.niel.hospital.management.system.controllers.PatientController;
+import com.dass.niel.hospital.management.system.controllers.StaffController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HospitalManagementSystemApplicationTests {
 
+	@Autowired
+	PatientController patientController;
+
+	@Autowired
+	StaffController staffController;
+
 	@Test
 	void contextLoads() {
+		assertThat(patientController).isNotNull();
+		assertThat(staffController).isNotNull();
 	}
 
 }
