f0826e8cf11f6241bdb66b06fe550e702998035b
Nielam Dass <nielam.dass@gmail.com>
2024-05-15 23:54:01 -0400

    Fixed query for method in visit repository

--- src/main/java/com/dass/niel/hospital/management/system/repositories/VisitRepository.java
+++ src/main/java/com/dass/niel/hospital/management/system/repositories/VisitRepository.java
@@ -3,13 +3,15 @@ package com.dass.niel.hospital.management.system.repositories;
 import com.dass.niel.hospital.management.system.entities.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
 
-    @Query("select v from visit v inner join v.patient p where (p.ssn = :ssn or :ssn is null) and " +
+    @Query("select v from Visit v inner join v.patient p where (p.ssn = :ssn or :ssn is null) and " +
             "(p.firstName = :firstName or :firstName is null) and (p.lastName = :lastName or :lastName is null) and " +
             "(v.admittedOn >= :fromDate or :fromDate is null) and (v.admittedOn <= :toDate or :toDate is null)")
     List<Visit> findBySsnNameAndAdmissionDateRange(Integer ssn, String firstName, String lastName, LocalDate fromDate, LocalDate toDate);
