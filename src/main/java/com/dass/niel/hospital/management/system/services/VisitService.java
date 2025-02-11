b491fe3c906c10cf5b03f3afd0f96eeb5bdaa30e
Nielam Dass <nielam.dass@gmail.com>
2025-02-11 17:49:44 -0500

    Converted VisitService to interface for dependency inversion

--- src/main/java/com/dass/niel/hospital/management/system/services/VisitService.java
+++ src/main/java/com/dass/niel/hospital/management/system/services/VisitService.java
@@ -2,72 +2,28 @@ package com.dass.niel.hospital.management.system.services;
 
 import com.dass.niel.hospital.management.system.entities.Staff;
 import com.dass.niel.hospital.management.system.entities.Visit;
-import com.dass.niel.hospital.management.system.repositories.VisitRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class VisitService {
+public interface VisitService {
+    public void addNewVisit(Visit visit);
 
-    @Autowired
-    VisitRepository visitRepository;
+    public void endVisit(Visit visit, LocalDate endDate);
 
-    public void addNewVisit(Visit visit){
-        visitRepository.save(visit);
-    }
+    public void assignVisitToStaff(Visit visit, Staff staff);
 
-    public void endVisit(Visit visit, LocalDate endDate){
-        visit.setDischargedOn(endDate);
-        visitRepository.save(visit);
-    }
+    public long getNumOfActiveVisits();
 
-    public void assignVisitToStaff(Visit visit, Staff staff){
-        List<Staff> staffInvolved = visit.getStaffInvolved();
-        for(Staff s : staffInvolved){
-            if(s.getStaffId().equals(staff.getStaffId())){
-                return;
-            }
-        }
-        staffInvolved.add(staff);
-        visit.setStaffInvolved(staffInvolved);
-        visitRepository.save(visit);
-    }
+    public List<Visit> getVisitsBySsnNameAndDateRange(Integer ssn, String firstName, String lastName, LocalDate fromDate, LocalDate toDate);
 
-    public long getNumOfActiveVisits(){
-        return visitRepository.countByDischargedOnNull();
-    }
+    public List<Visit> getActiveVisits();
 
-    public List<Visit> getVisitsBySsnNameAndDateRange(Integer ssn, String firstName, String lastName, LocalDate fromDate, LocalDate toDate){
-        return visitRepository.findBySsnNameAndAdmissionDateRange(ssn, firstName, lastName, fromDate, toDate);
-    }
+    public List<Visit> getVisitsByStaffInvolved(Long staffId);
 
-    public List<Visit> getActiveVisits(){
-        return visitRepository.findByDischargedOnNull();
-    }
+    public List<Visit> getActiveVisitsByStaffInvolved(Long staffId);
 
-    public List<Visit> getVisitsByStaffInvolved(Long staffId){
-        return visitRepository.findByStaffInvolved(staffId);
-    }
+    public List<Visit> getAllVisits();
 
-    public List<Visit> getActiveVisitsByStaffInvolved(Long staffId){
-        return visitRepository.findActiveByStaffInvolved(staffId);
-    }
-
-    public List<Visit> getAllVisits(){
-        return visitRepository.findAll();
-    }
-
-    public Visit getVisitById(Long visitId){
-        Optional<Visit> visit = visitRepository.findById(visitId);
-        if(visit.isPresent()){
-            return visit.get();
-        }
-        else{
-            return null;
-        }
-    }
+    public Visit getVisitById(Long visitId);
 }
---
+++ src/main/java/com/dass/niel/hospital/management/system/services/VisitServiceImpl.java
@@ -0,0 +1,72 @@
+package com.dass.niel.hospital.management.system.services;
+
+import com.dass.niel.hospital.management.system.entities.Staff;
+import com.dass.niel.hospital.management.system.entities.Visit;
+import com.dass.niel.hospital.management.system.repositories.VisitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VisitServiceImpl implements VisitService {
+    @Autowired
+    VisitRepository visitRepository;
+
+    public void addNewVisit(Visit visit){
+        visitRepository.save(visit);
+    }
+
+    public void endVisit(Visit visit, LocalDate endDate){
+        visit.setDischargedOn(endDate);
+        visitRepository.save(visit);
+    }
+
+    public void assignVisitToStaff(Visit visit, Staff staff){
+        List<Staff> staffInvolved = visit.getStaffInvolved();
+        for(Staff s : staffInvolved){
+            if(s.getStaffId().equals(staff.getStaffId())){
+                return;
+            }
+        }
+        staffInvolved.add(staff);
+        visit.setStaffInvolved(staffInvolved);
+        visitRepository.save(visit);
+    }
+
+    public long getNumOfActiveVisits(){
+        return visitRepository.countByDischargedOnNull();
+    }
+
+    public List<Visit> getVisitsBySsnNameAndDateRange(Integer ssn, String firstName, String lastName, LocalDate fromDate, LocalDate toDate){
+        return visitRepository.findBySsnNameAndAdmissionDateRange(ssn, firstName, lastName, fromDate, toDate);
+    }
+
+    public List<Visit> getActiveVisits(){
+        return visitRepository.findByDischargedOnNull();
+    }
+
+    public List<Visit> getVisitsByStaffInvolved(Long staffId){
+        return visitRepository.findByStaffInvolved(staffId);
+    }
+
+    public List<Visit> getActiveVisitsByStaffInvolved(Long staffId){
+        return visitRepository.findActiveByStaffInvolved(staffId);
+    }
+
+    public List<Visit> getAllVisits(){
+        return visitRepository.findAll();
+    }
+
+    public Visit getVisitById(Long visitId){
+        Optional<Visit> visit = visitRepository.findById(visitId);
+        if(visit.isPresent()){
+            return visit.get();
+        }
+        else{
+            return null;
+        }
+    }
+}
